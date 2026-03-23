bb0bbf93d0b23723d2aaa4459c4f608d8537f06e
Soupreet Dey <dey_soupreet@Soupreets-MacBook-Air.local>
2026-03-23 21:12:06 +0530

    Connect frontend to backend APIs

--- curewell-backend/src/main/java/com/curewell/hospital_management_system/entity/Doctor.java
+++ curewell-backend/src/main/java/com/curewell/hospital_management_system/entity/Doctor.java
@@ -16,13 +16,13 @@ public class Doctor {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "firstName", nullable = false)
+    @Column(name = "first_name", nullable = false)
     private String firstName;
 
-    @Column(name = "lastName", nullable = false)
+    @Column(name = "last_name", nullable = false)
     private String lastName;
 
-    @Column(name = "emailId", nullable = false, unique = true)
+    @Column(name = "email_id", nullable = false, unique = true)
     private String emailId;
 
     @ManyToMany
